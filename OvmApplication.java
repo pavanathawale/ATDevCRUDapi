c06605e1d843e547ac92c663abc6ed3bff9366c3
pavanathawale <134683767+pavanathawale@users.noreply.github.com>
2023-05-26 13:49:27 +0530

    Add files via upload

---
+++ OvmApplication.java
@@ -0,0 +1,13 @@
+package com.online.voting.OVM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OvmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OvmApplication.class, args);
+	}
+
+}
